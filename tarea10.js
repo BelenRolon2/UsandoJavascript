c0162c7d65501fbabada03d11d6fd890854626e0
BelenRolon2 <166760862+BelenRolon2@users.noreply.github.com>
2024-05-18 22:55:16 -0300

    Tarea N° 10
    Aplicando Javascript

---
+++ tarea10.js
@@ -0,0 +1,17 @@
+/* TAREA N° 10 -- EJERCICIO 1 */
+var nombre = "Belen";
+var apellido = "Rolon";
+
+console.log(nombre + " " + apellido);
+
+/* EJERCICIO 2 */
+
+var numero1 = 100;
+var numero2 = 200;
+var suma
+
+suma = numero1 + numero2;
+console.log(suma);
+
+var numero3 = suma + 50;
+console.log(numero3);
